bdfd92c803fd655c72511ae3b8fab9f6fb7f9615
Chris Breaux <christoperjbreaux@gmail.com>
2019-03-29 15:46:55 -0700

    display spash banner with selected city variable

--- src/app/city-splash/city-splash.component.ts
+++ src/app/city-splash/city-splash.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { City } from '../models/city.model';
 
 @Component({
   selector: 'app-city-splash',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-splash.component.scss']
 })
 export class CitySplashComponent implements OnInit {
+  @Input() childSelectedCity: City;
 
   constructor() { }
 
