4b3646e2cf3af5b851de74263948e26dcc0c06e1
Chris Breaux <christopherjbreaux@gmail.com>
2019-04-05 15:09:32 -0700

    add non-function dropdown

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { City } from '../models/city.model';
 import { CityService } from '../city.service';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -15,10 +16,11 @@ export class DropdownComponent implements OnInit {
   cityName: string = null;
   cityToDisplay: City;
   childCityList: City[];
+  cityForm: FormGroup;
 
 
 
-  constructor(private route: ActivatedRoute, private location: Location, private cityService: CityService) { }
+  constructor(private route: ActivatedRoute, private location: Location, private cityService: CityService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.childCityList = this.cityService.getCities();
@@ -26,6 +28,9 @@ export class DropdownComponent implements OnInit {
     this.route.params.forEach((urlParameters) => {
       this.cityName = urlParameters['city'];
       this.cityToDisplay = this.cityService.getCityByName(this.cityName)
+    });
+    this.cityForm = this.fb.group({
+      cityForm: [this.childCityList[0]]
     });
   }
 
