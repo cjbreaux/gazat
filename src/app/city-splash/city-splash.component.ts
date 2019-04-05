ba616b09930f71adb3a3a449b969798907277d42
Chris Breaux <christopherjbreaux@gmail.com>
2019-04-05 11:01:22 -0700

    refactor city splash to use new pathing

--- src/app/city-splash/city-splash.component.ts
+++ src/app/city-splash/city-splash.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit, Input} from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { City } from '../models/city.model';
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-city-splash',
   templateUrl: './city-splash.component.html',
-  styleUrls: ['./city-splash.component.scss']
+  styleUrls: ['./city-splash.component.scss'],
+  providers: [CityService]
 })
 export class CitySplashComponent implements OnInit {
-  @Input() childSelectedCity: City;
+  cityName: string = null;
+  cityToDisplay: City;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private cityService: CityService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.cityName = urlParameters['city'];
+    });
+    this.cityToDisplay = this.cityService.getCityByName(this.cityName)
   }
 
 }
